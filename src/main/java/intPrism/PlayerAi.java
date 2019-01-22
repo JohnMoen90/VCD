f1c600d5d4d6466ff41efac975ee14c4a19ada5b
JohnMoen90 <uk3718vi@go.minneapolis.edu>
2019-01-22 17:36:13 -0600

    Added playerAi class

---
+++ src/main/java/intPrism/PlayerAi.java
@@ -0,0 +1,19 @@
+package intPrism;
+
+public class PlayerAi extends CreatureAi {
+
+    public PlayerAi(Creature creature) {
+        super(creature);
+    }
+
+    // TODO: Add doors
+    public void onEnter(int x, int y, Tile tile){
+        if (tile.isGround()){
+            creature.x = x;
+            creature.y = y;
+        } else if (tile.isDiggable()) {
+            creature.dig(x, y);
+        }
+    }
+
+}
