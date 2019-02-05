656189e4b2178f6b7941acc7321c64e6f7fc1bd8
JohnMoen90 <uk3718vi@go.minneapolis.edu>
2019-02-05 13:17:18 -0600

    Added examine screen for items Added extremely basic item descriptions

--- src/main/java/VCD/screens/PlayScreen.java
+++ src/main/java/VCD/screens/PlayScreen.java
@@ -151,6 +151,7 @@ public class PlayScreen implements Screen {
             case KeyEvent.VK_D: subscreen = new DropScreen(player); break;
             case KeyEvent.VK_E: subscreen = new EatScreen(player); break;
             case KeyEvent.VK_W: subscreen = new EquipScreen(player); break;
+            case KeyEvent.VK_I: subscreen = new ExamineScreen(player); break;
 
         }
         }
